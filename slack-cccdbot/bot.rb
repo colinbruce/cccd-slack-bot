3f85b3e697a8c0286ee640d6507b0a73433f798c
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-11-17 15:20:01 +0000

    add help for details and branch

--- slack-cccdbot/bot.rb
+++ slack-cccdbot/bot.rb
@@ -1,4 +1,18 @@
 module SlackCccdbot
   class Bot < SlackRubyBot::Bot
+  	help do
+  		title 'LAA Get Paid Bot'
+  		desc 'This bot assists with the Get Paid Claim for Crown Court Defence application'
+
+  		command 'branch' do
+  			desc 'Shows the branch deployed on CCCD environments'
+  			long_desc 'Queries the given environment(s) and displays the branch name currently deployed'
+			end
+
+  		command 'details' do
+  			desc 'Shows the ping details page on CCCD environments'
+  			long_desc 'Queries the given environment(s) and displays the branch name currently deployed'
+			end
+  	end
   end
 end
