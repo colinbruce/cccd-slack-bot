551627d7d034e4af27013d38186fade302ac7171
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-11-17 15:31:14 +0000

    add help descriptions for planned commands

--- slack-cccdbot/bot.rb
+++ slack-cccdbot/bot.rb
@@ -4,14 +4,29 @@ module SlackCccdbot
   		title 'LAA Get Paid Bot'
   		desc 'This bot assists with the Get Paid Claim for Crown Court Defence application'
 
-  		command 'branch' do
-  			desc 'Shows the branch deployed on CCCD environments'
-  			long_desc 'Queries the given environment(s) and displays the branch name currently deployed'
+			command 'api' do
+				desc '`cccd api <environment>` e.g. `cccd api dev`'
+				long_desc 'Get a link to the api swagger docs for a given environment'
 			end
 
-  		command 'details' do
-  			desc 'Shows the ping details page on CCCD environments'
-  			long_desc 'Queries the given environment(s) and displays the branch name currently deployed'
+			command 'branch' do
+				desc '`cccd branch <environment>` e.g. `cccd branch api-sandbox`'
+				long_desc 'Shows the ping branch page for CCCD environment(s), accepts multiples e.g.  `cccd branch dev and api-sandbox` or `cccd branch dev, staging and production`'
+			end
+
+			command 'details' do
+				desc '`cccd details <environment>` e.g. `cccd details staging`'
+				long_desc 'Shows the ping details page for CCCD environment(s), accepts multiples e.g.  `cccd details dev and api-sandbox` or `cccd details dev, staging and production`'
+			end
+
+			command 'link' do
+				desc '`cccd link <environment>`, e.g. `cccd link production`'
+				long_desc 'Get a link to the login for a given environment'
+			end
+
+			command 'list' do
+				desc '`cccd list`'
+				long_desc 'Get a list of environments that can be passed to other commands'
 			end
   	end
 
