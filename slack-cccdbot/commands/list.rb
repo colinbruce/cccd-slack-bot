dc5a7b49ce2e9ff73552a3272ac680739f55fa3e
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-11-17 08:59:59 +0000

    expand list command

--- slack-cccdbot/commands/list.rb
+++ slack-cccdbot/commands/list.rb
@@ -1,8 +1,18 @@
 module SlackCccdbot
   module Commands
+
+    URI_SUFFIX = '.claim-crown-court-defence.service.justice.gov.uk'
+    NON_LIVE_ENVS = [ 'dev', 'staging', 'api-sandbox' ]
+    LIVE_ENV = ['cccd-production.apps.live-1.cloud-platform.service.justice.gov.uk']
+
     class List < SlackRubyBot::Commands::Base
-      command 'list' do |client, data, _match|
-        client.say(channel: data.channel, text: 'list environments')
+      command 'list' do |client, data, match|
+
+        match['expression'].split(/,\s|\s/).each do |env|
+          if NON_LIVE_ENVS.include?(env)
+            client.say(channel: data.channel, text: "Show data for `#{env}`")
+          end
+        end
       end
     end
   end
