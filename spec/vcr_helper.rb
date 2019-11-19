c03d4771f8fef3e91f2cfdcc941cadc3d8d870dd
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-11-19 16:08:36 +0000

    Remove rspec config section of vcr_helper

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -7,16 +7,3 @@ VCR.configure do |c|
   c.configure_rspec_metadata!
   c.debug_logger = $stderr if ENV['VCR_DEBUG']
 end
-
-# RSpec.configure do |config|
-#   config.around(:each, [:vcr_bot]) do |example|
-#     if VCR.turned_on?
-#       cassette = Pathname.new(example.metadata[:file_path]).cleanpath.sub_ext('').to_s
-#       VCR.use_cassette(cassette, :record => :new_episodes, :match_requests_on => [:method, :path_query_matcher]) do
-#         example.run
-#       end
-#     else
-#       example.run
-#     end
-#   end
-# end
