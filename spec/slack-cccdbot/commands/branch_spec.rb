f2ba39d9772a97c2efddddf2016037c26c3124d1
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-11-25 13:36:00 +0000

    extend branch tests and refactor tests+vcr

--- spec/slack-cccdbot/commands/branch_spec.rb
+++ spec/slack-cccdbot/commands/branch_spec.rb
@@ -1,18 +1,36 @@
 require 'spec_helper'
 
 describe SlackCccdbot::Commands::Branch, :vcr do
-  def app
-    SlackCccdbot::Bot.instance
+  subject { SlackCccdbot::Bot.instance }
+
+  let(:message) { "#{SlackRubyBot.config.user} branch #{env}" }
+  
+  context 'when user requests a single branch' do
+    let(:env) { 'dev' }
+
+    it 'returns the expected message' do
+      expect(message: message, channel: 'channel').to respond_with_slack_message('`dev` is running the `master` branch')
+    end
   end
 
-  subject { app }
+  context 'when user requests multiple branches' do
+    let(:env) { 'dev, staging and live' }
+    let(:response) do
+      [
+        "`dev` is running colinbruce's `VCR-test-branch` branch",
+        '`staging` is running the `master` branch',
+        '`live` is running the `master` branch'
+      ]
+    end
 
-  it 'returns the expected message' do
-    expect(message: "#{SlackRubyBot.config.user} branch dev", channel: 'channel').to respond_with_slack_message('`dev` is running the `master` branch')
+    it 'ignores formatting and displays expected messages' do
+      expect(message: message, channel: 'channel').to respond_with_slack_messages(response)
+    end
   end
+
 
   context 'when user requests all branches' do
-
+    let(:env) { 'all' }
     let(:response) do
       [
         "`dev` is running colinbruce's `VCR-test-branch` branch",
@@ -22,8 +40,8 @@ describe SlackCccdbot::Commands::Branch, :vcr do
       ]
     end
 
-  	it 'displays 5 messages' do
-      expect(message: "#{SlackRubyBot.config.user} branch all", channel: 'channel').to respond_with_slack_messages(response)
-  	end
+    it 'displays 5 messages' do
+      expect(message: message, channel: 'channel').to respond_with_slack_messages(response)
+    end
   end
 end
