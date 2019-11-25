ed8842f75671778e46ec414ef395ead90be6907b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-11-25 13:13:19 +0000

    add test for all envs

--- spec/slack-cccdbot/commands/branch_spec.rb
+++ spec/slack-cccdbot/commands/branch_spec.rb
@@ -10,4 +10,20 @@ describe SlackCccdbot::Commands::Branch, :vcr do
   it 'returns the expected message' do
     expect(message: "#{SlackRubyBot.config.user} branch dev", channel: 'channel').to respond_with_slack_message('`dev` is running the `master` branch')
   end
+
+  context 'when user requests all branches' do
+
+    let(:response) do
+      [
+        "`dev` is running colinbruce's `VCR-test-branch` branch",
+        '`staging` is running the `master` branch',
+        '`api-sandbox` is running the `master` branch',
+        '`production` is running the `master` branch'
+      ]
+    end
+
+  	it 'displays 5 messages' do
+      expect(message: "#{SlackRubyBot.config.user} branch all", channel: 'channel').to respond_with_slack_messages(response)
+  	end
+  end
 end
